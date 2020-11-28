462c919d31bb35bc43a1ef85a71e22bb36c801c6
wxl2 <1263311820@qq.com>
2020-11-28 18:14:39 +0800

    add json

---
+++ utils/ProtocolStream.cpp
@@ -0,0 +1,5 @@
+//
+// Created by wxl on 2020/11/27.
+//
+
+#include "protocolstream.h"
---
+++ utils/ProtocolStream.h
@@ -0,0 +1,15 @@
+//
+// Created by wxl on 2020/11/27.
+// C++二进制流协议类
+//
+
+#ifndef MAYA_PROTOCOLSTREAM_H
+#define MAYA_PROTOCOLSTREAM_H
+
+
+class protocolstream {
+
+};
+
+
+#endif //MAYA_PROTOCOLSTREAM_H
